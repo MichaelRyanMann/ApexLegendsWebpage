8f858b14af8758a3c63e489890a630ae75f05130
Michael Ryan Mann <Michryan25@gmail.com>
2020-06-26 18:31:04 -0500

    updated files

---
+++ legends.js
@@ -0,0 +1,58 @@
+// all legends individual outermost div container set to variables
+const bangalorePage = document.getElementById("Bangalore");
+const bloodhoundPage = document.getElementById("Bloodhound");
+const causticPage = document.getElementById("Caustic");
+const cryptoPage = document.getElementById("Crypto");
+const gibralterPage = document.getElementById("Gibralter");
+const lifelinePage = document.getElementById("Lifeline");
+const lobaPage = document.getElementById("Loba");
+const miragePage = document.getElementById("Mirage");
+const octanePage = document.getElementById("Octane");
+const pathfinderPage = document.getElementById("Pathfinder");
+const revenantPage = document.getElementById("Revenant");
+const wattsonPage = document.getElementById("Wattson");
+const wraithPage = document.getElementById("Wraith");
+
+// if/else if to check the window location hash path and then depending on hash, set dispay of that legends div to 'block' so it is visible
+    if (location.hash ==='#Bangalore') {
+        bangalorePage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Bloodhound'){
+        bloodhoundPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Caustic'){
+        causticPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Crypto'){
+        cryptoPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Gibralter'){
+        gibralterPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Lifeline'){
+        lifelinePage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Loba'){
+        lobaPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Mirage'){
+        miragePage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Octane'){
+        octanePage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Pathfinder'){
+        pathfinderPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Revenant'){
+        revenantPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Wattson'){
+        wattsonPage.style.display = "block";
+    }
+    else if(window.location.hash ==='#Wraith'){
+        wraithPage.style.display = "block";
+    }
+
+
+    
--- script.js
+++ script.js
@@ -1,31 +1,11 @@
+// assigning menu btn img and navBar to variables
 const navBar = document.getElementById("nav-bar");
 const menuIcon = document.getElementById("nav-toggle");
 
-/*let legendPoster = document.getElementsById("legend-poster").src;
-let legendHeadshot = document.getElementsById("legend-headshot");
-let saying = document.getElementsById("legend-saying").innerHTML;
-let nameHeader = document.getElementsById("name-header").innerHTML;
-let titleHeader = document.getElementsById("title-header").innerHTML;
-let story = document.getElementsById("leged-story").innerHTML;
-let legendName = document.getElementsById("legend-name").innerHTML;
-let legendAge = document.getElementsById("legend-age").innerHTML;
-let homePlanet = document.getElementsById("legend-planet").innerHTML;
-let nameOfTact = document.getElementsById("legend-tactical").innerHTML;
-let nameOfPass = document.getElementsById("legend-passive").innerHTML;
-let nameOfUlt = document.getElementsById("legend-ultimate").innerHTML;
-let tactImg = document.getElementsById("tact-img").src;
-let tactName = document.getElementsById("tact-name").innerHTML;
-let tactInfo = document.getElementsById("tact-info").innerHTML;
-let passImg = document.getElementsById("pass-img").src;
-let passName = document.getElementsById("pass-name").innerHTML;
-let passInfo = document.getElementsById("pass-info").innerHTML;
-let ultImg = document.getElementsById("ult-img").src;
-let ultName = document.getElementsById("ult-name").innerHTML;
-let ultInfo = document.getElementsById("ult-info").innerHTML;*/
-
-
+// setting navBar to display none so it is not visble before menu btn click
 window.onload = navBar.style.display = "none";
 
+// function to set navBar display to block when clicked and then back to display none when clicked again
 function menuClick(){    
     if(navBar.style.display === "none"){
         navBar.style.display = "block";
@@ -35,55 +15,5 @@ function menuClick(){
     }
 }
 
-/* legendPoster = "";
-legendHeadshot = "";
-saying = "";
-nameHeader = "";
-titleHeader = "";
-story = "";
-legendName = "";
-legendAge = "";
-homePlanet = "";
-nameOfTact = "";
-nameOfPass = "";
-nameOfUlt = "";
-tactImg = "";
-tactName = "";
-tactInfo = "";
-passImg = "";
-passName = "";
-passInfo = "";
-ultImg = "";
-ultName = "";
-ultInfo = "";
-
-
-
-
-
-function bloodhoundLinkClick(){
-    legendPoster = "images/BloodhoundPhotos/poster.jpg.adapt.crop16x9.1455w.jpg"; 
-    legendHeadshot = "images/BloodhoundPhotos/apex-grid-tile-legends-bloodhound.png.adapt.crop16x9.png";
-    saying = "I am the hunter the Gods have sent.";
-    nameHeader = "Bloodhound";
-    titleHeader = "Technological Tracker";
-    story = "Bloodhound is known across the Outlands as one of the greatest game hunters the Frontier has ever seen – and that’s about all anyone knows. Their identity is a mystery wrapped in layers of rumors: they are fabulously wealthy, a bloodthirsty murderer, a Goliath whisperer, a former slave, half bat, and a dozen other things depending on who’s doing the whispering.";
-    legendName = "Unknown";
-    legendAge = "Unknown";
-    homePlanet = "Unknown";
-    nameOfTact = "Eye of the Allfather";
-    nameOfPass = "Tracker";
-    nameOfUlt = "Beast of the Hunt";
-    tactImg = "images/BloodhoundPhotos/apex-grid-tile-legends-abilities-tactical-bloodhound.png.adapt.crop16x9.652w.png";
-    tactName = "Eye of the Allfather";
-    tactInfo = "Briefly reveal hidden enemies, traps, and clues throughout structures in front of you.";
-    passImg = "images/BloodhoundPhotos/apex-grid-tile-legends-abilities-passive-bloodhound.png.adapt.crop16x9.652w.png";
-    passName = "Tracker";
-    passInfo = "See tracks left behind by your foes.";
-    ultImg = "images/BloodhoundPhotos/apex-grid-tile-legends-abilities-ultimate-bloodhound.png.adapt.crop16x9.652w.png";
-    ultName = "Beast of the Hunt";
-    ultInfo = "Enhances your senses, allowing you to move faster and highlighting your prey.";
-}
-*/
 
 
